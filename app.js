618944677318e9ad6a6f4f1b1778b08ec41e4562
carinecroteau <croteau.carine@carrefour.cegepvicto.ca>
2019-04-30 12:59:16 -0400

    Ajout de la route jeux

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sallesRouter = require('./routes/salles');
 var pictosRouter = require('./routes/pictos');
+var jeuxRouter = require('./routes/jeux');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/salles', sallesRouter);
 app.use('/pictos', pictosRouter);
+app.use('/jeux', jeuxRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/jeux.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/*------------ Lisa ------------*/
+
+/**
+ * TODO:
+ * Obtention des dessins pour affichage à la fin du jeu.(READ)
+ * Mise à jour des scores. (UPDATE)
+ */
+
+/*------------ Sacha ------------*/
+
+/**
+ * TODO:
+ * Obtention des catégories pour affichage (READ)
+ * Obtention des niveaux pour affichage (READ)
+ * Création de catégories (CREATE)
+ *
+ */
+
+/*------------ Marc-Antoine ------------*/
+
+/**
+ * TODO:
+ * Obtention des catégories pour affichage (READ)
+ * Création des mots personnalisés (CREATE)
+ * Modifier les niveaux de difficulté des mots proposés.
+ *
+ */
+
+module.exports = router;
