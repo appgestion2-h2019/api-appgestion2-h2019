d5cd0982eb5cecc880d6302a326e70abc648de5d
Entrybot64 <entrybotjs@gmail.com>
2019-04-30 11:39:17 -0400

    Ajput filtre salles

--- routes/salles.js
+++ routes/salles.js
@@ -1,22 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-
 /*----------------------------------------*/
 /*-------------------- ÉTIENNE------------*/
 /*----------------------------------------*/
 
-
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+	res.send('respond with a resource');
 });
 
-router.post();
-
+//router.post();
 
 /*----------------------------------------*/
 /*-------------------- JULIEN-------------*/
 /*----------------------------------------*/
+
+router.get('/filtre', (req, res) => {
+	let filtres = ['min', 'max', 'type', 'langue'];
+	let parametres = Object.keys(req.query);
+
+	if(parametres.length !== 0) {
+		if(parametres.some(valeur => filtres.indexOf(valeur) >= 0)) {
+			res.end('ok');
+		} else {
+			res.end('not ok');
+		}
+	} else {
+		res.end('not ok');
+	}
+});
 
 module.exports = router;
