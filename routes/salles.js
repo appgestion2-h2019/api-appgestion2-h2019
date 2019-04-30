475cc819ed40ee95cec86d078f79b8f068da7e4e
Étienne Bouchard <etiennebouchard7@gmail.com>
2019-04-30 10:45:05 -0400

    Séparation du projet | Salles.js

--- routes/salles.js
+++ routes/salles.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
+/*----------------------------------------*/
+/*-------------------- ÉTIENNE------------*/
+/*----------------------------------------*/
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post();
+
+
+/*----------------------------------------*/
+/*-------------------- JULIEN-------------*/
+/*----------------------------------------*/
 
 module.exports = router;
