01e1cd2062cb82f2c446ce6744025ecf431044e7
Lisa <caissy.lisa@carrefour.cegepvicto.ca>
2019-05-02 10:25:47 -0400

    Mise à jour fichier jeux.js
    Ajout des deux requêtes d'affichage pour les pictos et l'update du score d'un utilisateur.

--- routes/jeux.js
+++ routes/jeux.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var express = require('express');
+var router = express.Router();
+var config = require('../../config');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = config.database.uri;
+const dbName = 'chickencoops';
+var ObjectId = require('mongodb').ObjectID;
+
 /*------------ Lisa ------------*/
 
 /**
@@ -8,6 +17,49 @@ var router = express.Router();
  * Obtention des dessins pour affichage à la fin du jeu.(READ)
  * Mise à jour des scores. (UPDATE)
  */
+//Requête pour l'affichage des pictos/dessins faits pendant la période de jeu.
+router.get('/', function(req, res, next) {
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        console.log("Connexion au serveur réussie");
+        const db = client.db(dbName);
+
+        //TODO mettre la collection picto
+        db.collection('METTRE LA COLLECTION DE PICTO').find().toArray(function(err, result) {
+
+            if (err) return console.log(err)
+            console.log(result);
+            res.json(result);
+        })
+
+        client.close();
+    });
+});
+
+//Requête pour la modification de la table 'usagers' et ajouter un score à l'utilisateur.
+router.put('/:idUsager', function(req, res, next) {
+    console.log("Mise à jour du score");
+
+    var idUsager = req.params.idUsager;
+    console.log(idUsager);
+
+    var objectScore = { score: req.body.score, usager_id: req.body.usager_id};
+    console.log(objectScore);
+
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        console.log("Connexion au serveur réussie");
+        const db = client.db(dbName);
+        db.collection('scores').updateOne({_id: ObjectId.createFromHexString(idUsager)},
+            {$set : objectScore}, function(err, result) {
+                if (err) return console.log(err)
+                console.log("Le score est à jour!");
+                res.json(result);
+            })
+
+        client.close();
+    });
+});
 
 /*------------ Sacha ------------*/
 
