d3f2c951987258fb9a190f16f24e2d6bcef7eb7d
Étienne Bouchard <etiennebouchard7@gmail.com>
2019-05-03 18:51:37 -0400

    Ajout des paramètres pour permettre le CORS dans salle.js

--- routes/salles.js
+++ routes/salles.js
@@ -12,6 +12,13 @@ var ObjectId = require('mongodb').ObjectID;
 /*-------------------- ÉTIENNE------------*/
 /*----------------------------------------*/
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 /**
  * TODO: 
  * Obtention des salles sans filtre
