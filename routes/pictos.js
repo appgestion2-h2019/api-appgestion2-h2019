4d95b5e4c6d5689fc525f525b915675aa3fe35a5
JimmyPellerin <jpellerin123@gmail.com>
2019-05-14 10:29:16 -0400

    Merge
    Merge

--- routes/pictos.js
+++ routes/pictos.js
@@ -1,17 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'index tout les picto' });
-});
-router.get('/ajouter', function(req, res, next) {
-    res.render('index', { title: 'ajouter pictos' });
-});
-router.get('/ajouter/:idpictos', function(req, res, next) {
-    res.render('index', { title: 'ajouter pictos' });
-});
+var config = require('../config');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = config.database.uri;
+const dbName = 'chickencoops';
+var ObjectId = require('mongodb').ObjectID;
 
+/**
+ *  AJOUTER UN PICTO
+ *  @desc Modifier un messager pour ajouter le nom du picto dans le message
+ *  @param int $idMessage - l'id du message dans le quelle ajouter un picto
+ *  @return le json du message
+ */
+router.put('/:idMessage', function (req, res, next) {
+                    //todo peut etre modifier ici
+    const picto = {titre: req.body.titre};
+    // console.log(picto);
 
+    if (!picto.titre) {
+        res.status(400);
+        res.json({"erreur": "Données incorrectes"});
+    } else {
+        MongoClient.connect(url, function (err, client) {
+            assert.equal(null, err);
+            console.log("Connexion au serveur réussie");
+            const db = client.db(dbName);
+
+            db.collection('').updateOne({_id: ObjectId.createFromHexString(req.params.idMessage)}, {$set: message},
+                function (err, result) {
+                    if (err) return console.log(err);
+                    console.log("Ajout picto dans le message");
+
+                    res.json(result);
+                    //res.render('films/Afficher', {films: result});
+                });
+            client.close();
+        });
+    }
+});
 
 module.exports = router;
