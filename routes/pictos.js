01ccdb15eed99be55ecc985c15183d8a2ebd7937
JimmyPellerin <jpellerin123@gmail.com>
2019-05-21 11:18:43 -0400

    function de sauvegarde

--- routes/pictos.js
+++ routes/pictos.js
@@ -23,8 +23,8 @@ router.get('/:idMessage', function (req, res, next) {
         assert.equal(null, err);
         console.log("Connexion au serveur réussie");
         const db = client.db(dbName);
-        db.collection('message').find().toArray(function(err, result)
-        // db.collection('').find({_id: ObjectId.createFromHexString(req.params.idMessage)}, {$set: message},
+        db.collection('message').find().toArray(
+            // db.collection('').find({_id: ObjectId.createFromHexString(req.params.idMessage)}, {$set: message},
             function (err, result) {
                 if (err) return console.log(err);
                 console.log("lecture du picto dans le message");
@@ -34,38 +34,37 @@ router.get('/:idMessage', function (req, res, next) {
             });
         client.close();
     });
+})
+/**
+ *  AJOUTER UN PICTO
+ *  @desc Modifier un messager pour ajouter le nom du picto dans le message
+ *  @param int $idMessage - l'id du message dans le quelle ajouter un picto
+ *  @return le json du message
+ */
+router.put('/:idMessage', function (req, res, next) {
+    //todo peut etre modifier ici
+    const picto = {titre: req.body.titre};
+    // console.log(picto);
 
-    /**
-     *  AJOUTER UN PICTO
-     *  @desc Modifier un messager pour ajouter le nom du picto dans le message
-     *  @param int $idMessage - l'id du message dans le quelle ajouter un picto
-     *  @return le json du message
-     */
-    router.put('/:idMessage', function (req, res, next) {
-        //todo peut etre modifier ici
-        const picto = {titre: req.body.titre};
-        // console.log(picto);
+    if (!picto.titre) {
+        res.status(400);
+        res.json({"erreur": "Données incorrectes"});
+    } else {
+        MongoClient.connect(url, function (err, client) {
+            assert.equal(null, err);
+            console.log("Connexion au serveur réussie");
+            const db = client.db(dbName);
 
-        if (!picto.titre) {
-            res.status(400);
-            res.json({"erreur": "Données incorrectes"});
-        } else {
-            MongoClient.connect(url, function (err, client) {
-                assert.equal(null, err);
-                console.log("Connexion au serveur réussie");
-                const db = client.db(dbName);
+            db.collection('').updateOne({_id: ObjectId.createFromHexString(req.params.idMessage)}, {$set: message},
+                function (err, result) {
+                    if (err) return console.log(err);
+                    console.log("Ajout picto dans le message");
 
-                db.collection('').updateOne({_id: ObjectId.createFromHexString(req.params.idMessage)}, {$set: message},
-                    function (err, result) {
-                        if (err) return console.log(err);
-                        console.log("Ajout picto dans le message");
-
-                        res.json(result);
-                        //res.render('films/Afficher', {films: result});
-                    });
-                client.close();
-            });
-        }
-    });
-
+                    res.json(result);
+                    //res.render('films/Afficher', {films: result});
+                });
+            client.close();
+        });
+    }
+});
     module.exports = router;
